49297020f2ef144f56d2ff41c00497fd96263458
Chaoyue_chen <Chaoyue_chen@coolermaster.com.cn>
2025-04-28 11:34:05 +0800

    fix:

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 
 mod utils;
-use utils::{get_mod_manifest, get_mod_latest_version};
+use utils::{get_mod_latest_version, get_mod_manifest};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
@@ -9,8 +9,8 @@ use utils::{get_mod_manifest, get_mod_latest_version};
 struct Mod {
     id: String,
     name: String,
-    version: String, 
-    latest_version: String,
+    version: String,
+    // latest_version: String,
 }
 
 #[tauri::command]
@@ -19,12 +19,13 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-async fn check_mods(directory: String) -> 
-Result<Vec<Mod>, String> {
+async fn check_mods(directory: String) -> Result<Vec<Mod>, String> {
     // 读取目录下的所有文件夹下面的 manifest.json 文件
-    let mut mods:Vec<Mod> = Vec::new();
+    let mut mods: Vec<Mod> = Vec::new();
     for entry in std::fs::read_dir(directory).unwrap() {
-        let result = get_mod_manifest(entry.unwrap().path().to_str().unwrap()).await.unwrap();
+        let result = get_mod_manifest(entry.unwrap().path().to_str().unwrap())
+            .await
+            .unwrap();
         // println!("Found mod: {}", result);
         // 使用正则表达式匹配 mod 的id、名称和版本号
         let name_re = Regex::new(r#""Name":\s*"(.*?)"\s*,"#).unwrap();
@@ -33,9 +34,13 @@ Result<Vec<Mod>, String> {
 
         let is_num_re = Regex::new(r"^\d+$").unwrap();
 
-        let name = name_re.captures(&result).map(|c| c.get(1).unwrap().as_str());
+        let name = name_re
+            .captures(&result)
+            .map(|c| c.get(1).unwrap().as_str());
         let id = id_re.captures(&result).map(|c| c.get(1).unwrap().as_str());
-        let version = version_re.captures(&result).map(|c| c.get(1).unwrap().as_str());
+        let version = version_re
+            .captures(&result)
+            .map(|c| c.get(1).unwrap().as_str());
 
         // println!("Found mod: {} {} {}", name.unwrap_or(""), id.unwrap_or(""), version.unwrap_or(""));
         if name.is_some() && id.is_some() && version.is_some() && is_num_re.is_match(id.unwrap()) {
@@ -43,7 +48,7 @@ Result<Vec<Mod>, String> {
                 id: id.unwrap().to_string(),
                 name: name.unwrap().to_string(),
                 version: version.unwrap().to_string(),
-                latest_version: version.unwrap().to_string(),
+                // latest_version: version.unwrap().to_string(),
             });
         }
     }
@@ -56,34 +61,33 @@ Result<Vec<Mod>, String> {
     //         mod_.latest_version = version.clone();
     //     }
     // }
-    let mod_futures = mods.iter().map(|mod_| {
-        let mod_id = mod_.id.clone();
-        async move {
-            match get_mod_latest_version(&mod_id).await {
-                Ok((_, version)) => Some((mod_id, version)),
-                Err(e) => {
-                    eprintln!("获取最新版本失败: {}", e);
-                    None
-                }
-            }
-        }
-    });
+    // let mod_futures = mods.iter().map(|mod_| {
+    //     let mod_id = mod_.id.clone();
+    //     async move {
+    //         match get_mod_latest_version(&mod_id).await {
+    //             Ok((_, version)) => Some((mod_id, version)),
+    //             Err(e) => {
+    //                 eprintln!("获取最新版本失败: {}", e);
+    //                 None
+    //             }
+    //         }
+    //     }
+    // });
 
-    let results: Vec<Option<(String, String)>> = futures::future::join_all(mod_futures).await;
+    // let results: Vec<Option<(String, String)>> = futures::future::join_all(mod_futures).await;
 
-    for mod_ in mods.iter_mut() {
-        if let Some((_, version)) = results.iter().find_map(|res| {
-            res.as_ref().filter(|(id, _)| *id == mod_.id)
-        }) {
-            if mod_.version != *version {
-                mod_.latest_version = version.clone();
-            }
-        }
-    }
+    // for mod_ in mods.iter_mut() {
+    //     if let Some((_, version)) = results.iter().find_map(|res| {
+    //         res.as_ref().filter(|(id, _)| *id == mod_.id)
+    //     }) {
+    //         if mod_.version != *version {
+    //             mod_.latest_version = version.clone();
+    //         }
+    //     }
+    // }
 
     Ok(mods)
 }
-
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useMemo } from "react";
 import reactLogo from "./assets/react.svg";
-import { Button, Table } from "antd";
+import { Button, Table, TableColumnsType, Spin } from "antd";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import { open } from '@tauri-apps/plugin-dialog';
@@ -9,6 +9,8 @@ interface Mod {
   id: string; // 唯一标识符
   name: string; // 名称
   version: string; // 版本 
+  latest_version?: string; // 最新版本
+  // description: string; // 描述
 }
 
 function App() {
@@ -55,12 +57,16 @@ function App() {
     }
   }
 
-  const columns = useMemo(() => [
+  function check_mods(mods: Mod[]) {
+
+  }
+
+  const columns = useMemo<TableColumnsType>(() => [
     { title: 'ID', dataIndex: 'id', key: 'id' },
     { title: '名称', dataIndex: 'name', key: 'name' },
     { title: '当前版本', dataIndex: 'version', key: 'version' },
-    { title: '最新版本', dataIndex: 'latest_version', key: 'latest_version' },
-    { title: '操作', key: 'action', render: () => <Button type="primary">下载</Button> }
+    { title: '最新版本', dataIndex: 'latest_version', key: 'latest_version', render: (value) => <>{value ? <span>{value}</span> : <Spin />}</> },
+    { title: '操作', key: 'action', render: (_, record) => <Button type="primary" disabled={!record['latest_version'] && record['latest_version'] !== record.version}>下载</Button> }
   ], []);
 
   return (
